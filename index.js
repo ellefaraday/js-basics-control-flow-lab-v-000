022a9d4818f8390ce13b40396ae00f8779389eeb
Elle Faraday <faraday.elle@gmail.com>
2018-07-23 10:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ function ternaryCheckCity(city) {
 
 function switchOnCharmFromTip(tip) {
   let message
-  case 'generous':
-    message = 'Thank you so much.';
-    break;
-  // case 'not as generous':
-  //   message = 'Thank you.';
-  //   break;
-  // default:
-  //   message = "Bye."
-  //   break;
-  // return message
+  switch (tip) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = "Bye."
+      break;
+    return message}
 }
