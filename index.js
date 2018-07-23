4aed11cf1cc585437c8e10d5802e542d69838b58
Elle Faraday <faraday.elle@gmail.com>
2018-07-23 10:39:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Write your code in this file!
 function scuberGreetingForFeet (distance) {
   let message
-  if (distance < 400) {
-    message = 
+  if (distance > 2500) {
+    message = 'No can do.'; 
+  } else if (distance > 2000) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (distance < 400) {
+    message = 'This one is on me!';
   }
 
 }
